67748ce654680208290acade60501e34d9a54006
Bruno Carneiro <bcarneiro.externo@creditoagricola.pt>
2024-09-21 11:01:29 +0100

    init project

---
+++ src/main/java/com/mycleaner_authentication/MyCleanerAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.mycleaner_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCleanerAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCleanerAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycleaner_authentication/MyCleanerAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycleaner_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCleanerAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
